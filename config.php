cc5a53bb3002d520dea494ff5f297aa41403974a
IBelonogov <job@belonogov.com>
2022-09-04 23:32:16 +1100

    +++ white-foot template

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+// Source of Your RAML file (local or http)
+$RAMLsource = 'raml/twitter.raml';
+
+// Types of Action Verbs Allowed
+$RAMLactionVerbs = array('get', 'post', 'put', 'patch', 'delete', 'connect', 'trace');
+
+// APC Cache Time Limit - set to "0" to disable
+$cacheTimeLimit = '36000';
+
+// Path to the theme file for the docs
+$docsTheme = 'templates/white-foot/index.phtml';
+
+// Enable Try It (alpha)
+$tryIt = false;
+
+// Enable Disqus
+$disqus = false;
+
+// Disqus Short Name for Site (see Disqus admin)
+$disqus_shortname = '';
+
+?>
